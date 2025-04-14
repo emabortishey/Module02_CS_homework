ae4ffcf9ecd52c7e619c84b80408099b66dcac0d
fartb <fartbreather228@gmail.com>
2025-04-15 02:23:34 +0300

    1 задание: выполнены арифметические задания с массивами

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -59,3 +60,45 @@ for (int i = 0; i < B.GetLength(0); i++)
     }
     WriteLine();
 }
+
+double summD = 0;
+double maxD = B[0, 0];
+double multD = 1;
+double unevenCOLsummD = 0;
+
+int evensumm = 0;
+
+// для нахождения суммы элементов, нечетных столбцов, произведения и макс. числа я просто использовала 1 цикл 
+// т.к. лень было искать функции для матриц или пытаться эксперементировать с aggregate 
+for (int i = 0; i < B.GetLength(0); i++)
+{
+    for (int j = 0; j < B.GetLength(1); j++)
+    {
+        summD += B[i, j];
+        multD *= B[i, j];
+
+        if (B[i, j] > maxD)
+        {
+            maxD = B[i, j];
+        }
+
+        if(i%2!=0)
+        {
+            unevenCOLsummD += B[i, j];
+        }
+    }
+}
+
+for(int i = 0; i < A.Length; i+=2)
+{
+    evensumm += A[i];
+}
+
+WriteLine($"Максимальный элемент массива А: {A.Max()}");
+WriteLine($"Максимальный элемент массива B: {maxD}");
+WriteLine($"Сумма всех элементов массива А: {A.Aggregate((x, y) => x + y)}");
+WriteLine($"Сумма всех элементов массива B: {summD}");
+WriteLine($"Произведение всех элементов массива А: {A.Aggregate((x, y) => x * y)}");
+WriteLine($"Произведение всех элементов массива B: {multD}");
+WriteLine($"Сумма всех элементов с чётным индексом массива А: {evensumm}");
+WriteLine($"Сумма всех элементов столбцов с нечётным индексом массива B: {unevenCOLsummD}");
